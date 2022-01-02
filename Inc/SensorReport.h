910c318222b8bba2f10e08c809e68e810235f9a9
joseluu <jose.luu@gmail.com>
2022-01-02 05:39:40 +0100

    initial commit as generated by cubeMX and imported

---
+++ Inc/SensorReport.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef __SENSORREPORT_H
+#define __SENSORREPORT_H
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	void initializeSensorReport();
+	void doReport();
+#ifdef __cplusplus
+}
+#endif
+
+#endif
